b6c89dba65ebd51d806b02219c0ffed96b7ff870
michaelahli <michael.ahli99@gmail.com>
2020-05-25 20:35:54 +0700

    deploy heroku

--- config/configuration.js
+++ config/configuration.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoDbUrl: 'mongodb://localhost:27017/cmstutorial',
+    mongoDbUrl: 'mongodb+srv://michaelahli:koko0410@cluster0-osdpx.mongodb.net/test?retryWrites=true&w=majority',
     PORT: process.env.PORT || 3000,
     globalVariables: (req, res, next) => {
         res.locals.success_message = req.flash('success-message');
